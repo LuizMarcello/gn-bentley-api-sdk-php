8c107d59a05e5e741f99a4beace3b113cae66572
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-09-21 10:01:42 -0300

    Londrina:21.09.2021_10:01hs

--- codigo-fonte/indexflexbox.php
+++ codigo-fonte/indexflexbox.php
@@ -5,11 +5,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Flexbox</title>
+  <title>Hotel Paraíso</title>
   <!-- Icones fontawesome: -->
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-   integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-    crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
   <!-- Fontes da google: font-family: 'Open Sans', sans-serif; -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 
@@ -31,6 +29,23 @@
       Article 04
     </article>
   </section> -->
+
+  <header>
+    <a href="#"><img src="../imgFlexBox/logo.png" alt="Hotel Paraíso"></a>
+
+    <nav>
+      <li><a href="">Home</a></li>
+      <li><a href="">Sobre</a></li>
+      <li><a href="">Reservas</a></li>
+      <li><a href="">Contato</a></li>
+    </nav>
+  </header>
+
+  <section class="hero">
+    <h1>Conheça este paraíso chamado Itacaré</h1>
+    <h3>"Aconchego, confôrto, simpatia e alto astral"</h3>
+    <a href="" class="btn">Saiba mais</a>
+  </section>
 
 </body>
 
