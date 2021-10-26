3edb35cfe04fce3ec3f56ae4ccdb3fee64e9384e
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-10-26 09:49:19 -0300

    Rolandia:26.10.2021_09:49hs

--- codigo-fonte/cadastrar.php
+++ codigo-fonte/cadastrar.php
@@ -2,8 +2,8 @@
 require_once 'classes/usuarios.php';
 if (!isset($_SESSION)) session_start();
 $u = new Usuario;
-$u->conectar("gerencianet_usuarios", "localhost", "root", "P@ssw0rd");
-/* $u->conectar("gerencianet_usuarios", "localhost", "root", "root1234"); */
+/* $u->conectar("gerencianet_usuarios", "localhost", "root", "P@ssw0rd"); */
+$u->conectar("gerencianet_usuarios", "localhost", "root", "root1234");
 
 ?>
 
