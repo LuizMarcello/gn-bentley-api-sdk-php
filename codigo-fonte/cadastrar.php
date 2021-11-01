39c895ff48471843f2ba02b0f638cb8b174a9044
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-11-01 15:50:20 -0300

    01.11.2021_15:50hs

--- codigo-fonte/cadastrar.php
+++ codigo-fonte/cadastrar.php
@@ -14,8 +14,10 @@ $u->conectar("gerencianet_usuarios", "localhost", "root", "P@ssw0rd");
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+   rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
+    crossorigin="anonymous">
 
   <link rel="stylesheet" href="css/style.css">
  
@@ -28,7 +30,8 @@ $u->conectar("gerencianet_usuarios", "localhost", "root", "P@ssw0rd");
   <header>
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse"
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+         data-bs-target="#navbarCollapse"
           aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -48,7 +51,7 @@ $u->conectar("gerencianet_usuarios", "localhost", "root", "P@ssw0rd");
 
   <main>
     <div class="flex-box container-box">
-      <div class="content-box">
+      <div class="content-box"> 
         <h1>Cadastrar</h1>
         <form method="POST">
           <input type="text" name="nome" placeholder="Nome Completo" maxlength="45">
