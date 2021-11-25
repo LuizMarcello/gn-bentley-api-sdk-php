c5a3dea43e8ee35d2709b496d248fcbb975fab86
luizmarcello <luizmarcello.vmo@hotmail.com>
2021-11-25 11:24:26 -0300

    Rolândia:25.11.2021_11:24hs

--- index.php
+++ index.php
@@ -15,13 +15,8 @@ $u->conectar("gerencianet_usuarios", "localhost", "root", "root1234");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bentley Brasil</title>
     <!-- Icones fontawesome: -->
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
-<<<<<<< HEAD
-    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" 
-    crossorigin="anonymous" />
-=======
-integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
->>>>>>> 5ea0e81b21f7f4391c1abb298ce21c947b9c3f3d
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <!-- Fontes da google: font-family: 'Open Sans', sans-serif; -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
     <link rel="stylesheet" href="css/styleindex.css">
@@ -39,10 +34,10 @@ integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg
                 <li><a style="white-space: nowrap;" href="usuarioLogado.php">Meu Perfil</a></li>
                 <li>
                     <?php if (isset($_SESSION['id'])) { ?>
-                        <a class="nav-link" href="sair.php">Sair</a>
+                    <a class="nav-link" href="sair.php">Sair</a>
                     <?php } ?>
                     <?php if (!isset($_SESSION['id'])) { ?>
-                        <a class="nav-link" href="logar.php">Logar</a>
+                    <a class="nav-link" href="logar.php">Logar</a>
                     <?php } ?>
                 </li>
 
@@ -57,10 +52,10 @@ integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg
 
                     if ($sql->rowCount() > 0) {
                         $dado = $sql->fetch(); ?>
-                        <li>
-                            <a style="white-space: nowrap;" class="nav-link"><?php echo $dado['nome']; ?> </a>
-                        </li>
-                    <?php } ?>
+                <li>
+                    <a style="white-space: nowrap;" class="nav-link"><?php echo $dado['nome']; ?> </a>
+                </li>
+                <?php } ?>
                 <?php } ?>
 
             </div>
@@ -71,44 +66,33 @@ integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg
         <img src="img/cabecalhositebentley.jpg" alt="Bentley Brasil">
     </section>
 
-    
-
-
-    <section id="imagensjuruena">
-        <section class="img01">
-            <img src="img/juruena05.jpg" alt="Bentley Brasil">
-        </section>
-        <section class="img02">
-            <img src="img/juruena03.jpg" alt="Bentley Brasil">
-        </section>
-        <section class="img03">
-            <img src="img/juruena04.jpg" alt="Bentley Brasil">
-        </section>
-        <section class="img04">
-            <img src="img/juruena01.jpg" alt="Bentley Brasil">
-        </section>
 
 
 
-
-
-       <!--  <ul class="imgjuruena1">
-            <div class="img01">
-                <img src="img/juruena05.jpg" alt="Bentley Brasil">
+    <section class="imagensjuruena">
+<h1>Equipamentos Bentley</h1>
+        <div class=imagensjuruena-container>
+            <div class="produtos-item img01">
+                <h2>Powerbean M5</h2>
+                <img src="img/juruena05.jpg" alt="Imagem 1">
             </div>
-            <div class="img03">
-                <img src="img/juruena03.jpg" alt="Bentley Brasil">
+            <div class="produtos-item img02">
+                <h2>Powerbeam ac</h2>
+                <img src="img/juruena03.jpg" alt="Imagem 2">
             </div>
-        </ul>
-        <ul class="imgjuruena2">
-            <div class="img04">
-                <img src="img/juruena04.jpg" alt="Bentley Brasil">
+            <div class="produtos-item img03">
+                <h2>TP-Link Tl-wr840n</h2>
+                <img src="img/juruena04.jpg" alt="Imagem 3">
             </div>
-            <div class="img05">
-                <img src="img/juruena01.jpg" alt="Bentley Brasil">
+            <div class="produtos-item img04">
+                <h2>Liteabeam ac</h2>
+                <img src="img/juruena01.jpg" alt="Imagem 4">
             </div>
-        </ul> -->
+        </div>
     </section>
+
+
+
 
     <section class="reserva">
         <a href="indexcomprar.php"><img src="img/fazerreserva.jpg"></a>
