50f205be4f1b417bbdef2fed13aa28b52bf8fd93
luizmarcello <luizmarcello.vmo@hotmail.com>
2022-01-28 18:23:53 -0300

    Londrina:28.01.2022_18:23hs

--- esqueciSenha/alterar-senha.php
+++ esqueciSenha/alterar-senha.php
@@ -7,11 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Alterar Senha</title>
   <!-- Icones fontawesome: -->
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
   <!-- Fontes da google: font-family: 'Open Sans', sans-serif; -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
   <link rel="stylesheet" href="../esqueciSenha/css/styleindex.css">
 </head>
@@ -37,29 +35,33 @@
     $chave = preg_replace('/[^[:alnum:]]/', '', $_GET["chave"]);
   ?>
 
-  <form id="alterarSenha" action="set-nova-senha.php" method="POST">
-    <input type="hidden" name="chave" value="<?php echo $chave; ?>" />
+    <main>
+      <div class="content-box">
+        <form id="alterarSenha" action="set-nova-senha.php" method="POST">
+          <input type="hidden" name="chave" value="<?php echo $chave; ?>" />
 
-    <h1>Alteração da senha</h1>
+          <h1>Alteração da senha</h1>
 
-    <label>Email</label><br>
-    <input type="text" name="email" /><br><br>
+          <label>Email</label><br>
+          <input type="text" size="37" name="email" /><br><br>
 
-    <label>Nova Senha</label><br>
-    <input type="password" name="senha" id="senha" /><br>
-    <button onclick="mostrarASenha()" type="button" id="mostrarrSenha" class="btn btn-primary botao btn-sm">
-      Mostrar Senha</button><br><br>
+          <label>Nova Senha</label><br>
+          <input type="password" size="37" name="senha" id="senha" /><br>
+          <button onclick="mostrarASenha()" type="button" id="mostrarrSenha" class="btn btn-primary botao btn-sm">
+            Mostrar Senha</button><br><br>
 
-    <label>Repita a nova senha</label><br>
-    <input type="password" name="repetesenha" id="repetesenha" /><br>
-    <button onclick="mostrarRepeteSenha()" type="button" id="repetirSenha" class="btn btn-primary botao btn-sm">
-      Mostrar Senha</button><br><br>
+          <label>Repita a nova senha</label><br>
+          <input type="password" size="37" name="repetesenha" id="repetesenha" /><br>
+          <button onclick="mostrarRepeteSenha()" type="button" id="repetirSenha" class="btn btn-primary botao btn-sm">
+            Mostrar Senha</button><br><br>
 
-    <!-- <input type="password" name="senha_usuario" id="senha_usuario" placeholder="Senha" maxlength="45"> -->
-    <!-- <button onclick="mostrarASenha()" type="button" id="mostrarrSenha" class="btn btn-primary botao btn-sm">Mostrar Senha</button> -->
+          <!-- <input type="password" name="senha_usuario" id="senha_usuario" placeholder="Senha" maxlength="45"> -->
+          <!-- <button onclick="mostrarASenha()" type="button" id="mostrarrSenha" class="btn btn-primary botao btn-sm">Mostrar Senha</button> -->
 
-    <input type="submit" class="btn btn-primary botao btn-sm" value="Enviar" maxlength="45">
-  </form>
+          <input type="submit" class="btn btn-primary botao btn-sm" value="Enviar" maxlength="45">
+        </form>
+      </div>
+    </main>
 
   <?php
   } else {
--- esqueciSenha/envia-nova-senha.php
+++ esqueciSenha/envia-nova-senha.php
@@ -7,11 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Enviar nova senha</title>
   <!-- Icones fontawesome: -->
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-    integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
   <!-- Fontes da google: font-family: 'Open Sans', sans-serif; -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
   <link rel="stylesheet" href="../esqueciSenha/css/styleindex.css">
 </head>
@@ -33,32 +31,37 @@
   require_once '../classes/usuarios.php';
   if (!isset($_SESSION)) session_start();
   $u = new Usuario;
- /* $u->conectar("gerencianet_usuarios", "localhost", "root", "P@ssw0rd"); */
-   $u->conectar("gerencianet_usuarios", "localhost", "root", "root1234");
-
-if(isset($_POST["email"])){
-
-$email = $_POST["email"];
-
-  $email = preg_replace('/[^[:alnum:]_.-@]/', '', $email);
-
-  $chave = $u->geraChaveAcesso($email);
-
-  if ($chave) {
-    /* echo '<a href="http://localhost/gn-bentley-api-sdk-php/esqueciSenha/alterar-senha.php?chave=' . $chave . '">
-http://localhost/gn-bentley-api-sdk-php/esqueciSenha/alterar-senha.php?chave=' . $chave . '</a>'; */
-echo '<a href="http://localhost/gn-bentley-api-sdk-php/esqueciSenha/alterar-senha.php?chave=' . $chave . '">
-Clique aqui para alterar sua senha criptografada</a>';
-        
-  } else {
-    echo '<h1>Êrro: Usuário não encontrado.</h1>';
-  }
-}else {
-?>
-  <!-- <h3>Você não informou nenhum e-mail</h3> -->
-  <?php
-}
+  $u->conectar("gerencianet_usuarios", "localhost", "root", "P@ssw0rd");
+  /*  $u->conectar("gerencianet_usuarios", "localhost", "root", "root1234"); */
   ?>
+
+  <main>
+    <div class="content-box">
+      <?php
+      if (isset($_POST["email"])) {
+
+        $email = $_POST["email"];
+
+        $email = preg_replace('/[^[:alnum:]_.-@]/', '', $email);
+
+        $chave = $u->geraChaveAcesso($email);
+
+        if ($chave) {
+          /* echo '<a href="http://localhost/gn-bentley-api-sdk-php/esqueciSenha/alterar-senha.php?chave=' . $chave . '">
+http://localhost/gn-bentley-api-sdk-php/esqueciSenha/alterar-senha.php?chave=' . $chave . '</a>'; */
+          echo '<a href="http://localhost/gn-bentley-api-sdk-php/esqueciSenha/alterar-senha.php?chave=' . $chave . '">
+Clique aqui para alterar sua senha criptografada</a>';
+        } else {
+          echo '<h1>Êrro: Usuário não encontrado.</h1>';
+        }
+      } else {
+      ?>
+        <!-- <h3>Você não informou nenhum e-mail</h3> -->
+      <?php
+      }
+      ?>
+    </div>
+  </main>
 
   <footer>
     <ul>
